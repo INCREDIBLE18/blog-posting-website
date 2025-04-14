dde8a5688f058e29757f411c58e8829245bc1b29
Deepak Gavhane <71230832+INCREDIBLE18@users.noreply.github.com>
2025-04-14 23:19:36 +0530

    vite.config.js

--- blog-frontend/vite.config.js
+++ blog-frontend/vite.config.js
@@ -1,9 +1,11 @@
+// blog-frontend/vite.config.js
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()], // Removed tailwindcss()
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
